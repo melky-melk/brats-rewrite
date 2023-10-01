ae61fe2f1c7347d217ed9116d9609743a4f69aaa
melky-melk <chiara.justine.tan@gmail.com>
2023-10-01 22:17:59 +1100

    messing with prep.py

--- prep.py
+++ prep.py
@@ -3,15 +3,15 @@ print("Downloadning BraTS2016/17")
 from monai.apps import DecathlonDataset
 from generative.losses.perceptual import PerceptualLoss
 
-_ = DecathlonDataset(root_dir="/mnt/Datasets/Open-Datasets/MONAI", task="Task01_BrainTumour", section="training", download=True)
-# _ = DecathlonDataset(root_dir="/mnt/.node1/Open-Datsets/MONAI", task="Task01_BrainTumour", section="training", download=True)
-
 perceptual_loss = PerceptualLoss(
     spatial_dims=3, 
     network_type="squeeze", 
     is_fake_3d=True, 
     fake_3d_ratio=0.2
 )
+
+_ = DecathlonDataset(root_dir="/mnt/Datasets/Open-Datasets/MONAI", task="Task01_BrainTumour", section="training", download=True, cache_rate = 0.0)
+# _ = DecathlonDataset(root_dir="/mnt/.node1/Open-Datsets/MONAI", task="Task01_BrainTumour", section="training", download=True)
 
 # # Download the bigger dataset from Synapse
 # print("Downloadning BraTS2023")
