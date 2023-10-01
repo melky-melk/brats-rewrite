8e5cb16654f1ce1f747f1525179599492599d069
melky-melk <chiara.justine.tan@gmail.com>
2023-10-01 15:53:13 +1100

    perceptual loss changes

--- prep.py
+++ prep.py
@@ -7,7 +7,10 @@ from generative.losses.perceptual import PerceptualLoss
 _ = DecathlonDataset(root_dir="/mnt/.node1/Open-Datsets/MONAI", task="Task01_BrainTumour", section="training", download=True)
 
 perceptual_loss = PerceptualLoss(
-    spatial_dims=2, network_type="resnet50", pretrained=True, #ImageNet pretrained weights used
+    spatial_dims=3, 
+    network_type="squeeze", 
+    is_fake_3d=True, 
+    fake_3d_ratio=0.2
 )
 
 # # Download the bigger dataset from Synapse
